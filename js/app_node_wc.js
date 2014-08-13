b5009eeed8a92a5924cfeb5aeaca06c1c4a5f4f3
Ryan Holder <ryan@ryanholder.com>
2014-08-13 14:17:03 +0100

    Using SSL certificate for basic Auth
    Setup dev to use SSL for basic auth

--- js/app_node_wc.js
+++ js/app_node_wc.js
@@ -1,6 +1,6 @@
 var WC = require( 'woocommerce-rest-api' );
 var wc = new WC({
-    endpoint: 'http://wp.thewhatwhat.com/wc-api/v1/',
+    endpoint: 'https://wp.thewhatwhat.com/wc-api/v1/',
     username: 'ck_45841d89825d617a00814f88e74face7',
     password: 'cs_d6da0b74e1f26cdd1f6bb6c8a0207e90',
     auth: true
