863de8f041320843fcbc8daaa27d79809f4c07f4
Ryan Holder <ryan@ryanholder.com>
2014-08-19 17:26:10 +0100

    Implement directive test example
    Test creating sparkline for weather: http://www.ng-newsletter.com/posts/directives.html

--- js/directives.js
+++ js/directives.js
@@ -8,4 +8,47 @@ angular.module('woodash.directives', [])
 		return function (scope, elm, attrs) {
 			elm.text(version);
 		};
-	}]);
+	}])
+
+    .directive('woodashPieChart', function () {
+        return {
+            restrict: 'EA',
+            template: '<div class="sparkline"></div>'
+        }
+    })
+
+.directive('ngSparkline', function() {
+        var url = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=imperial&cnt=14&callback=JSON_CALLBACK&q=";
+        return {
+            restrict: 'A',
+            require: '^ngCity',
+            scope: {
+                ngCity: '@'
+            },
+            template: '<div class="sparkline"><h4>Weather for {{ngCity}}</h4><div class="graph"></div></div>',
+            controller: ['$scope', '$http', function($scope, $http) {
+                $scope.getTemp = function(city) {
+                    $http({
+                        method: 'GET',
+                        url: url + city
+                    }).success(function(data) {
+                        console.log(data);
+                        var weather = [];
+                        angular.forEach(data.list, function(value){
+                            weather.push(value);
+                        });
+                        $scope.weather = weather;
+                    });
+                }
+            }],
+            link: function(scope, iElement, iAttrs, ctrl) {
+                scope.getTemp(iAttrs.ngCity);
+            }
+        }
+})
+
+.directive('ngCity', function() {
+    return {
+        controller: function($scope) {}
+    }
+});
