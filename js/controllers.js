9ca498a38cd0a94589e97d1d16069fc65b579640
Ryan Holder <ryan@ryanholder.com>
2014-10-02 17:58:17 +0100

    Add modal for cloud connect

--- js/controllers.js
+++ js/controllers.js
@@ -12,15 +12,48 @@ angular.module('woodash.controllers', [])
 
     })
 
-    .controller('DashboardCtrl', ['$scope', 'initDashboard', function ($scope, initDashboard) {
+    .controller('DashboardCtrl', ['$scope', 'initDashboard', '$ionicModal', function ($scope, initDashboard, $ionicModal) {
         this.googleConnected = initDashboard.googleAuth.isAuthenticated;
         this.dropboxConnected = initDashboard.dropboxAuth.isAuthenticated;
+
+        $ionicModal.fromTemplateUrl('templates/modals/dashboard.cloud.connect.html', {
+            scope: $scope,
+            animation: 'slide-in-up',
+            backdropClickToClose: false,
+            hardwareBackButtonClose: false
+        }).then(function(modal) {
+            $scope.modal = modal;
+            modal.show();
+        });
+
+        $scope.openModal = function() {
+            $scope.modal.show();
+        };
+
+        $scope.closeModal = function() {
+            $scope.modal.hide();
+        };
+
+        //Cleanup the modal when we're done with it!
+        $scope.$on('$destroy', function() {
+            $scope.modal.remove();
+        });
+
+        // Execute action on hide modal
+        $scope.$on('modal.hidden', function() {
+            // Execute action
+        });
+
+        // Execute action on remove modal
+        $scope.$on('modal.removed', function() {
+            // Execute action
+        });
 
         if (!this.googleConnected && !this.dropboxConnected) {
             //perform the modal popup/welcome with request to connect to one of the services
             console.log('we need to connect to a service');
+            //$scope.modal.show();
         }
-
     }])
 
 	.controller('OverviewCtrl', ['$scope', 'initOverview', function ($scope, initOverview) {
