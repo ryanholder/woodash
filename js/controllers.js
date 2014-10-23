48a1f1d8c1219f443ea848d1baac6978e1185a8a
Ryan Holder <ryan@ryanholder.com>
2014-10-23 13:49:31 +0100

    Get images for the customers using http getting blob

--- js/controllers.js
+++ js/controllers.js
@@ -112,7 +112,103 @@ angular.module('woodash.controllers', [])
         //console.log(this.orders);
 	}])
 
-    .controller('CustomersCtrl', ['$scope', function ($scope) {
+    .controller('CustomersCtrl', ['$scope', '$http', 'wcCustomers', function ($scope, $http, wcCustomers) {
+        //var customers = this;
+
+        var params = {
+            "filter[limit]": 99
+        };
+
+
+
+        $scope.getCustomerImage = function(url) {
+            //console.log(url);
+            $http.get(url, {responseType: 'blob'}).success(function(blob, status, headers, config) {
+
+                console.log(blob, status, headers, config);
+
+                //blob.name = doc.iconFilename; // Add icon filename to blob.
+
+                //writeFile(blob); // Write is async, but that's ok.
+
+                var localImage = window.URL.createObjectURL(blob);
+                //return localImage;
+
+                //dataset.avatar_url_local = localImage;
+                //$scope.docs.push(doc);
+
+                // Only sort when last entry is seen.
+                //if (totalEntries - 1 == i) {
+                //    $scope.docs.sort(Util.sortByDate);
+                //}
+            });
+
+            //return url;
+        };
+
+        wcCustomers.getList(params).then(function(results) {
+            console.log(results);
+
+            $scope.list = results;
+
+            //for (var i = results.length - 1; i >= 0; i--) {
+            //    $scope.list.push(results.plain()[i]);
+            //    var dataset = results[i];
+            //    var remoteImage = dataset.avatar_url;
+
+                //var xhr = new XMLHttpRequest();
+                //xhr.open('GET', remoteImage, true);
+                //xhr.responseType = 'blob';
+                //xhr.onload = function(e) {
+                    //var img = document.createElement('img');
+                    //console.log(window.URL.createObjectURL(this.response));
+                    //dataset.avatar_url = window.URL.createObjectURL(this.response);
+                    //document.body.appendChild(img);
+                //};
+
+                //xhr.send();
+
+                //remoteImage = new RAL.RemoteImage(dataset.avatar_url);
+                //RAL.Queue.add(remoteImage);
+                //remoteImage.addEventListener('loaded', function(remoteImage) {
+                //    console.log(remoteImage);
+                //});
+                //if (dataset.window && dataset.window === 'lifetime') {
+                //    delete dataset.window;
+                //    retObj = dataset;
+                //    break;
+                //}
+                //console.log($scope.list);
+            //}
+            //RAL.Queue.setMaxConnections(4);
+            //RAL.Queue.start();
+            $scope.list.forEach(function(record) {
+                $http.get(record.avatar_url, {responseType: 'blob'}).success(function(blob, status, headers, config) {
+
+                    console.log(blob, status, headers, config);
+
+                    record.avatar_url = window.URL.createObjectURL(blob);
+                    //blob.name = doc.iconFilename; // Add icon filename to blob.
+
+                    //writeFile(blob); // Write is async, but that's ok.
+
+                    //var localImage = window.URL.createObjectURL(blob);
+                    //return localImage;
+
+                    //dataset.avatar_url_local = localImage;
+                    //$scope.docs.push(doc);
+
+                    // Only sort when last entry is seen.
+                    //if (totalEntries - 1 == i) {
+                    //    $scope.docs.sort(Util.sortByDate);
+                    //}
+                });
+                console.log(record);
+            });
+        })
+
+
+
 
     }])
 
